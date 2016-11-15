4ddd4dfef363c082e92c8d2d7d8221f52e63f6b0
Jo <thenoblejmn@gmail.com>
2016-11-15 15:13:32 -0800

    Participation

--- demo-3/server.R
+++ demo-3/server.R
@@ -5,7 +5,7 @@ shinyServer(function(input, output) {
   
   # Reder a histogram of a given color
   output$histogram <- renderPlot({
-    x <- rnorm(1000)
+    x <- rnorm(input$num)
     return(hist(x, col = input$color))
   })
 })
